24ae86f4cfaf3f4aa45165c4cedb49b49c8d70df
sandile-dev01 <sandilegofx@gmail.com>
2023-08-18 15:03:49 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        int temperature = 31;
+
+        if (temperature > 30){
+            System.out.println("It's a hot day, drink plenty of water");
+        }else if(temperature > 20 && temperature < 30){
+            System.out.println("It's a nice day");
+        }else{
+            System.out.println("Its cold");
+        }
+    }
+}
