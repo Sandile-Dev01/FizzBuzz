18172c5786e75442bd73ccb0c860b9fe228acd36
sandile-dev01 <sandilegofx@gmail.com>
2023-08-18 16:06:11 +0200

    made FizzBuzz

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        int temperature = 31;
+     Scanner scanner = new Scanner(System.in);
 
-        if (temperature > 30){
-            System.out.println("It's a hot day, drink plenty of water");
-        }else if(temperature > 20 && temperature < 30){
-            System.out.println("It's a nice day");
-        }else{
-            System.out.println("Its cold!");
+     System.out.print("Number: ");
+
+     int guess = scanner.nextInt();
+
+     //Check if number is divisible by both 3 & 5
+     if(guess % 3 == 0 && guess % 5 == 0){
+            System.out.println("FizzBuzz");
         }
+     //Check if number is devisible by 3
+     else if (guess % 3 == 0){
+         System.out.println("Fizz");
+     }
+
+     //Check if number is devisible by 5
+     else if(guess % 5 == 0){
+         System.out.println("Buzz");
+     }
+
+     else{
+         System.out.println(guess);
+     }
     }
 }
